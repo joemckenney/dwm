c15ca91f414a8bc0bcc84a681d3c8655a00ff209
joemckenney <j.mckenney00@gmail.com>
2019-12-08 17:41:22 -0800

    Map print screen key to scrot

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[] = "-*-liberation mono-bold-*-*-*-12-*-*-*-*-*-*-0";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -65,6 +65,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ 0,                            XK_Print,  spawn,          SHCMD("sleep .25s; pushd ~/scrots; scrot -s; popd;") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
